6fac07dc4da4d3f4a9fbd063351842862b8b670d
Luciano <lucianorepetto07@gmail.com>
2025-07-22 21:24:03 -0300

    public src

---
+++ src/data/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCu_mdr8yTU-nZaOWZw3GVwiVlwaqvMdZc",
+  authDomain: "adimensionalshop.firebaseapp.com",
+  projectId: "adimensionalshop",
+  storageBucket: "adimensionalshop.appspot.com",
+  messagingSenderId: "719117032303",
+  appId: "1:719117032303:web:df4a6876bfd5001b3ca69a"
+});
+
+const messaging = firebase.messaging();
